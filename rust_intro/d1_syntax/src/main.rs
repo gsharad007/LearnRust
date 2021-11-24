50fe2a0bfe39c33f2d9a30230be2c74a3bad1956
THESHREDDER\TheBeast <sharad@unknownworlds.com>
2021-11-24 15:48:37 -0500

    Finished Strust and Enums lesson

--- rust_intro/d1_syntax/src/main.rs
+++ rust_intro/d1_syntax/src/main.rs
@@ -1,4 +1,29 @@
+#[derive(Debug, PartialEq)]
+pub struct Point {
+    x: i32,
+    y: i32,
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Ship {
+    location: Point,
+    status: ShipStatus,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum ShipStatus {
+    Engaged,
+    Waiting,
+    Firing(Point),
+    Heading(Point),
+}
+
 fn main() {
-    let x:i32 = 3;
-    println!("{}", x);
+    let mut ship = Ship {
+        location: Point { x: 10, y: 10 },
+        status: ShipStatus::Waiting,
+    };
+    ship.location.x += 10;
+    println!("ship location = ({}, {})", ship.location.x, ship.location.y);
+    println!("ship = {:?}", ship);
 }
