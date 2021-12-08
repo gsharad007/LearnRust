6c52f4bfb479420dceffdc2b41f15268c54d75cb
THESHREDDER\TheBeast <sharad@unknownworlds.com>
2021-12-08 18:01:13 -0500

    Updating Add-ProjectWorkflows to be more dynamic

--- rust_intro/Add-ProjectWorkflows.ps1
+++ rust_intro/Add-ProjectWorkflows.ps1
@@ -1,28 +1,36 @@
 
 
-$s = (
-    'rust_intro/d1_syntax',
-    'rust_intro/d1_syntax_generics',
-    'rust_intro/d1_syntax_loops',
-    'rust_intro/d1_syntax_result',
-    'rust_intro/d2_lifetimes_array',
-    'rust_intro/d2_lifetimes_box',
-    'rust_intro/d2_lifetimes_matter',
-    'rust_intro/d2_lifetimes_passing',
-    'rust_intro/d2_lifetimes_refcounter',
-    'rust_intro/d2_lifetimes_str',
-    'rust_intro/d3_tricks_errors',
-    'rust_intro/d3_tricks_mod',
-    'rust_intro/d3_tricks_newtype',
-    'rust_intro/d3_tricks_poly',
-    'rust_intro/d3_tricks_trait',
-    'rust_intro/v1_hello'
-)
+# $s = (
+#   'rust_intro/d1_syntax',
+#   'rust_intro/d1_syntax_generics',
+#   'rust_intro/d1_syntax_loops',
+#   'rust_intro/d1_syntax_result',
+#   'rust_intro/d2_lifetimes_array',
+#   'rust_intro/d2_lifetimes_box',
+#   'rust_intro/d2_lifetimes_matter',
+#   'rust_intro/d2_lifetimes_passing',
+#   'rust_intro/d2_lifetimes_refcounter',
+#   'rust_intro/d2_lifetimes_str',
+#   'rust_intro/d3_tricks_errors',
+#   'rust_intro/d3_tricks_mod',
+#   'rust_intro/d3_tricks_newtype',
+#   'rust_intro/d3_tricks_poly',
+#   'rust_intro/d3_tricks_trait',
+#   'rust_intro/v1_hello'
+# )
 
-$s | % {
-    $filename = $_.Replace('/','_')
-    $data = @"
-name: $_
+Push-Location $PSScriptRoot
+
+try {
+  $rootFolder = Split-Path $PSScriptRoot -Parent
+  $projectFolders = Get-ChildItem -Directory
+  $projectFolders | ForEach-Object {
+    if (-not (Test-Path "$($_.FullName)\Cargo.toml")) { continue; }
+    $projectFolder = $_.FullName.Replace("$rootFolder\", '').Replace('\', '/')
+    Write-Output $projectFolder
+    $filename = $projectFolder.Replace('/', '_')
+    @"
+name: $projectFolder
 
 on:
   push:
@@ -35,7 +43,7 @@ env:
 
 defaults:
   run:
-    working-directory: ./$_
+    working-directory: ./$projectFolder
 
 jobs:
   build-window:
@@ -58,5 +66,9 @@ jobs:
       run: cargo build --verbose
     - name: Build Release
       run: cargo build --release --verbose
-"@.trim() | Set-Content ".\.github\workflows\$filename.yml"
+"@.trim() | Set-Content "..\.github\workflows\$filename.yml"
+  }
+}
+finally {
+  Pop-Location
 }
